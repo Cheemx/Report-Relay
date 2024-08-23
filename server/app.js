7455a9212628552c63d9352ace4c4ed2b0147f44
Cheemx <chinmaymahajan999@gmail.com>
2024-08-23 20:46:13 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "*",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }))
