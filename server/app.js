789f2181e9044c22e951295006234b1c1fcffe80
Cheemx <chinmaymahajan999@gmail.com>
2024-08-23 17:19:03 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "*",
     credentials: true
 }))
 app.use(express.json({
